4151974d29ba7af0400a3346e6051f17558de17f
George Kudryavtsev <egorkudr1@gmail.com>
2016-03-05 20:40:46 +0300

    homework done

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# This function creates a list with 4 methods.
+# method sets take matrix into a list.
+# method gets give matrix from a list.
+# method setinv takes iverse matrix into a list.
+# method getinv gives inverse matrix from a list.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set=set, get=get, setinv = setinv, getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
-
+# This function takes x of special type, which was created with makeCacheMatrix, and return an inverse matrix.
+# If this function calculated an inverse matrix for x before it won't calculate again it. 
+# and  will make message "getting cached data". 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  inv <- solve(x$get(), ...)
+  x$setinv(inv)
+  inv
 }
